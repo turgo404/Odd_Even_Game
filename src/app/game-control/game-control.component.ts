316fb24cddc7a7407d0c7753930e439c2c8284ec
Turgay Orhanov <Turgod@abv.bg>
2023-06-23 19:09:27 +0300

    Finished

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent {
+  @Output() intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber = 0;
+  @ViewChild('startGameButton', { static: true }) startGameButton;
+
+
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000);
+    this.startGameButton.nativeElement.disabled = true;
+  }
+
+  onPauseGame() {
+    clearInterval(this.interval);
+    this.startGameButton.nativeElement.disabled = false;
+  }
+
 
 }
